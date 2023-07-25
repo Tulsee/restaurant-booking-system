1b461364085a3d832ccbe1df05b9a6730597fd8c
tulsee <ramtulsi022@gmail.com>
2023-07-25 16:27:37 +0545

    data fetched in server component

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { MenuCard } from ".";
+import { Item } from "@prisma/client";
 
-export default function Menu() {
+export default function Menu({ menu }: { menu: Item[] }) {
   return (
     <main className="bg-white mt-5">
       <div>
         <div className="mt-4 pb-1 mb-1">
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
-        <div className="flex flex-wrap justify-between">
-          <MenuCard />
-        </div>
+        {menu.length ? (
+          <div className="flex flex-wrap justify-between">
+            {menu.map((item) => (
+              <MenuCard key={item.id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-between">
+            <p>This Restaurant does not have menu</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -8,16 +8,50 @@ import {
   Reviews,
   Title,
 } from "./components";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error();
+  }
+  return restaurant;
+};
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  const { name, images, description, slug } = restaurant;
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={slug} />
+        <Title name={name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={description} />
+        <Images images={images} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
